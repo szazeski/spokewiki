94aa2f89dafbfd39d8a2a9b0dac5d4f1c7484942
Steve Zazeski <steve@zazeski.com>
2022-11-06 21:10:41 -0600

    build and deploy script

---
+++ build-and-deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e # fail on errors
+
+echo "- - - - - - - - - - - - - - - "
+echo "building..."
+
+npm run build
+
+cd build
+
+echo "deploying..."
+
+aws s3 sync . s3://spokewiki.com/
+aws cloudfront create-invalidation --distribution-id E3HIBICDGM1H56 --path /*
+
+echo "check out https://www.spokewiki.com/"
