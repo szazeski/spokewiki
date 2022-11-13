7f1ee7e0ad625c0a909db9d611305955addd95d4
Steve Zazeski <steve@zazeski.com>
2022-11-12 20:08:40 -0600

    better audio player layout

--- src/components/spokeAudioPlayer/index.js
+++ src/components/spokeAudioPlayer/index.js
@@ -1,4 +1,4 @@
-import H5AudioPlayer from "react-h5-audio-player";
+import H5AudioPlayer, {RHAP_UI} from "react-h5-audio-player";
 import style from "./style.css";
 
 const SpokePlayer = ({src, title, onStop}) => {
@@ -37,17 +37,32 @@ const SpokePlayer = ({src, title, onStop}) => {
             autoPlay={false}
             autoPlayAfterSrcChange={true}
             src={src}
+            layout="stacked-reverse"
             onPlay={onStart}
             onPause={onPause}
             onEnded={onEnd}
             customAdditionalControls={
                 [
-                    <button class={style.SpokeAudioPlayerSpeed} onClick={() => setSpeed(1)}>1x</button>,
+                    <button class={style.SpokeAudioPlayerSpeed} onClick={() => setSpeed(1)}>1.0x</button>,
                     <button class={style.SpokeAudioPlayerSpeed} onClick={() => setSpeed(1.2)}>1.2x</button>,
                     <button class={style.SpokeAudioPlayerSpeed} onClick={() => setSpeed(1.5)}>1.5x</button>,
                 ]
             }
             customVolumeControls={[]}
+            customControlsSection={
+                [
+                    RHAP_UI.ADDITIONAL_CONTROLS,
+                    RHAP_UI.MAIN_CONTROLS,
+                    RHAP_UI.VOLUME_CONTROLS,
+                ]
+            }
+            customProgressBarSection={
+                [
+                    RHAP_UI.CURRENT_TIME,
+                    RHAP_UI.PROGRESS_BAR,
+                    RHAP_UI.DURATION
+                ]
+            }
             style={style.SpokeAudioPlayer}
         />
     )
