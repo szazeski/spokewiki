34751b106f498d19d9c6b723ae46f2bea2c9929e
Steve Zazeski <steve@zazeski.com>
2022-11-17 23:28:14 -0600

    set theme color, add articles

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -7,6 +7,7 @@ const About = () => (
         <p>spokewiki tracks current events and pulls the wikipedia article, does minor editing to improve readability
             then uses Amazon Polly to voice the article. We try our best to be fair, non-partisan and open to
             feedback.</p>
+        <a href="mailto:feedback@spokewiki.com">feedback@spokewiki.com</a>
     </div>
 );
 
--- src/routes/settings/index.js
+++ src/routes/settings/index.js
@@ -3,15 +3,33 @@ import style from './style.css';
 
 const Settings = () => {
 
+    let root = document.documentElement;
+
+    function setThemeColor(color) {
+        console.log(color.target.style.backgroundColor);
+        setColor(color.target.style.backgroundColor);
+    }
+
+    function setColor(rgb) {
+        root.style.setProperty('--primary', rgb);
+        let metaThemeColor = document.querySelector("meta[name=theme-color]");
+        metaThemeColor.setAttribute("content", rgb);
+    }
+
     return (
         <div class={style.settings}>
             <p>coming soon...</p>
-            <p>App Color : <span className={style.topic}>Purple</span></p>
+            <div>App Color</div>
+            <div class={style.theme}>
+                <span style="background-color:#57129d" onClick={setThemeColor}>Purple</span>
+                <span style="background-color:#333" onClick={setThemeColor}>Grey</span>
+                <span style="background-color:#0000ff" onClick={setThemeColor}>Blue</span>
+            </div>
             <p>Light/Dark Mode : Auto</p>
             <p>Default Speed: 1.5x</p>
             <div>Never show :
                 <span class={style.topic}>Politics</span>
-                <span className={style.topic}>Crypto</span>
+                <span class={style.topic}>Crypto</span>
             </div>
         </div>
     );
