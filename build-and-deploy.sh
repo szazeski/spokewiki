5165e7a54c9f8e9b50a4a7397c7bec344c88f363
Steve Zazeski <steve@zazeski.com>
2023-01-01 12:05:34 -0600

    extract env vars

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e # fail on errors
 
+if [ -z "$S3PATH" ]; then
+  echo "Env var S3PATH is not set (export S3PATH=s3://bucketname)"
+  exit 1
+fi
+
 echo "- - - - - - - - - - - - - - - "
 echo "building..."
 
@@ -10,8 +15,15 @@ cd build
 
 echo "deploying..."
 
-aws s3 sync . s3://spokewiki.com/
-aws cloudfront create-invalidation --distribution-id E3HIBICDGM1H56 --path "/*" > /dev/null
+aws s3 sync . "$S3PATH"
+
+# check if $AWS_CLOUDFRONT_DISTRIBUTION_ID is set
+if [ -z "$AWS_CLOUDFRONT_DISTRIBUTION_ID" ]; then
+  echo "Env var AWS_CLOUDFRONT_DISTRIBUTION_ID is not set (export AWS_CLOUDFRONT_DISTRIBUTION_ID=your_distribution_id)"
+else
+  aws cloudfront create-invalidation --distribution-id $AWS_CLOUDFRONT_DISTRIBUTION_ID --path "/*" > /dev/null
+fi
+
 
 echo ""
 echo "uploaded finished, check out https://www.spokewiki.com/"
