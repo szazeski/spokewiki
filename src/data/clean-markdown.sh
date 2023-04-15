77a49b5e69e1b9a4421bc122bae2ce5f4fb8edf9
Steve Zazeski <steve@zazeski.com>
2023-04-15 13:53:47 -0500

    New articles and automation to pull articles (mostly)

---
+++ src/data/clean-markdown.sh
@@ -0,0 +1,33 @@
+
+
+if [ -f "$1" ]; then
+    echo "File $1 exists"
+else
+    echo "File $1 does not exist"
+    exit 1
+fi
+
+# newlines
+sed -i '' 's/\\n/\n/g' "$1"
+
+# headers
+sed -i '' 's/=//g' "$1"
+
+# quotes
+sed -i '' 's/\\"/"/g' "$1"
+
+# unicodes
+sed -i '' 's/\\u2013/-/g' "$1"
+sed -i '' 's/\\u2014/-/g' "$1"
+sed -i '' 's/\\u00a3/£/g' "$1"
+sed -i '' 's/\\u2019/’/g' "$1"
+sed -i '' 's/\\u2018/‘/g' "$1"
+sed -i '' 's/\\u201c/“/g' "$1"
+sed -i '' 's/\\u201d/”/g' "$1"
+sed -i '' 's/\\u2026/.../g' "$1"
+sed -i '' 's/\\u00a0/ /g' "$1"
+
+# add space after period if followed by a letter
+sed -i '' 's/\.\([a-zA-Z]\)/. \1/g' "$1"
+
+echo "done"
--- src/data/get-article.sh
+++ src/data/get-article.sh
@@ -1,20 +1,32 @@
 
-echo "enter a wikipedia url or slug"
-
-read -r SLUG
-
-SLUG="Kyrsten_Sinema"
+SLUG=$1
+if [ -z "$SLUG" ]; then
+  echo "enter a wikipedia url or slug (https://en.wikipedia.org/wiki/SLUG)?"
+  read -r SLUG
+  SLUG=${SLUG/https://en.wikipedia.org/wiki//} # removes url part from slug
+fi
 
 rm output.json
 curl -o output.json "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=${SLUG}&explaintext&format=json"
 
-PAGEID=$(cat output.json | jq '..|.pageid?')
-TITLE=$(cat output.json | jq ".query.pages.${PAGEID}.title")
-TEXT=$(cat output.json | jq ".query.pages.${PAGEID}.extract")
+PAGEID=$(jq '..|.pageid? | select( . != null)' output.json)
+if [ -z "$PAGEID" ]; then
+  echo "Unable to get pageid"
+  exit 1
+fi
+echo "PageID is $PAGEID"
+
+TITLE=$(jq ".query.pages.\"${PAGEID}\".title" output.json)
+if [ -z "$TITLE" ]; then
+  echo "Unable to get page title"
+  exit 1
+fi
+echo "Title is $TITLE"
 
 FILENAME="articles/${SLUG}.md"
-echo "${TEXT}" > "$FILENAME"
-
+jq ".query.pages.\"${PAGEID}\".extract" output.json > "$FILENAME"
 echo "Thanks for listening to ${TITLE} on spokewiki recorded on ${TODAY}" >> "$FILENAME"
-
 echo "$PAGEID" >> "$FILENAME"
+
+echo "cleaning markdown"
+./clean-markdown.sh "$FILENAME"
